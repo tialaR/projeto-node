ffb207eed26f446791f0f55213c997c5b99439cb
Tiala Rocha <tialarocha@gmail.com>
2021-02-21 21:10:59 -0300

    Listando agendamentos

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -5,6 +5,12 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 const appointmentRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
+appointmentRouter.get('/', (request, response) => {
+    const appointments = appointmentsRepository.all();
+
+    return response.json(appointments);
+});
+
 appointmentRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
 
